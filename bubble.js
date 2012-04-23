69db66e7e6c2685fd2670e68e0fe9e0531c16387
IMAZU Asami <neko.daisuki.070109@gmail.com>
2012-04-23 18:40:10 +0900

    bubble.js 下の余白はなぜ……

---
+++ bubble.js
@@ -0,0 +1,34 @@
+var five = require('../node-five'),
+    yoko = 300, tate = 200
+    window = new five.Window(yoko, tate),
+    canvas = new five.Canvas(window),
+    ctx = canvas.getContext('2d');
+
+var rr = function(min,max){
+  var range = max+1 - min;
+  return Math.floor(Math.random() * range) + min;
+}
+
+var color = function(){
+  switch(rr(1,5)){
+    case 1:
+      return 'darkblue';
+    case 2:
+      return 'midiumblue';
+    case 3:
+      return 'dodgerblue';
+    case 4:
+      return 'lightskyblue';
+    case 5:
+      return 'aliceblue';
+  }
+};
+
+var bgImage = new five.Image();
+bgImage.src = __dirname + '/images/sea.png';
+ctx.drawImage(bgImage,0,0);
+
+var timer = setInterval(function(){
+  ctx.fillStyle = color();
+  ctx.fillText(rr(0,1)==0?'o':'O', rr(1,yoko), rr(1,tate));
+},100);
