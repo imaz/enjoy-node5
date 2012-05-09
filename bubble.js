3f5775c279c4e8616c77c6d52d68a6e0394e514e
IMAZU Asami <neko.daisuki.070109@gmail.com>
2012-05-09 12:25:07 +0900

    コマンド引数をチェックする部分をクラスの外に出した #3

--- bubble.js
+++ bubble.js
@@ -1,8 +1,23 @@
-
 var error_usage = function(){
   console.error('Usage: node bubble.js [--color (blue|red|yellow|green)]');
   process.exit(1);
 };
+
+var color = new Color();
+var args = process.argv.slice(2);
+while(args.length){
+  switch(args.shift()){
+    case '-c':
+    case '--color':
+      color.index = color.set.indexOf(args.shift());
+      if(color.index == -1)
+        error_usage();
+      break;
+    default:
+      error_usage();
+      break;
+  }
+}
 
 var rr = function(min,max){
   var range = max+1 - min;
@@ -10,30 +25,15 @@ var rr = function(min,max){
 };
 
 function Color(){
-  this.kind_index = 0;
-  var args = process.argv.slice(2);
-  this.kind_set = ['blue','red','yellow','green'];
-  while(args.length){
-    switch(args.shift()){
-      case '-c':
-      case '--color':
-        this.kind_index = this.kind_set.indexOf(args.shift());
-        if(this.kind_index == -1)
-          error_usage();
-        break;
-      default:
-        error_usage();
-        break;
-    }
-  }
-  var color_detail = ['darkblue','midiumblue','dodgerblue','lightskyblue','aliceblue'];
+  this.index = 0;
+  this.set = ['blue','red','yellow','green'];
+  var detail = ['darkblue','midiumblue','dodgerblue','lightskyblue','aliceblue'];
   this.detail= function(){
-    return color_detail[rr(1,5)+this.kind_index*5];
+    return detail[rr(1,5)+this.index*5];
   };
 };
 
 function Bubble(){
-  var color = new Color();
   var five = require('node-five'),
       yoko = 300, tate = 200,
       window = new five.Window(yoko, tate),
